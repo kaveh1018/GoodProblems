87f0a45c36333c8860a99a5150512fd79fd4099e
kaveh mohammadpour <mp.kaveh@gmail.com>
2019-10-15 03:31:00 -0400

    Update superIterator.swift
    Array Extension

--- superIterator.swift
+++ superIterator.swift
@@ -9,32 +9,37 @@
 import Foundation
 
 
-let arr = [1,2,3,[1.1,[4.3,5,[1:"test","car":"bood"]],2,[]]] as [Any]
+let arr = [1,2,3,[1.1,[4.3,5,[1:"test","car":"bood"]],2,[],nil]] as [Any]
 
-func superItratpr<T>(_ arr:T, _ visit:(Any)->Void){
+extension Array {
+
+    func iterateAny(_ visit:(Any)->Void){
+        superItratpr(self, visit)
+    }
     
-    guard let num = arr as? Array<Any>  else {
-        guard let dic = arr as? [AnyHashable:Any] else {
-            visit(arr)
-            return
-        }
+    private func superItratpr<T>(_ arr:T , _ visit:(Any)->Void){
         
-        dic.forEach { (arg0) in
+        guard let num = arr as? Array<Any>  else {
+            guard let dic = arr as? [AnyHashable:Any] else {
+                visit(arr)
+                return
+            }
             
-            let (key, value) = arg0
-            print("Key:\(key), Value:\(value)")
+            dic.forEach { visit($0)
+            }
+            return
+            
         }
-        return
-        
-    }
 
-    num.forEach { (el) in
-        superItratpr(el,visit)
+        num.forEach { (el) in
+            superItratpr(el,visit)
+        }
+      
     }
-  
 }
 
-superItratpr(arr){ el in
+
+arr.iterateAny { (el) in
     print(el)
 }
 
